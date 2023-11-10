adea166f095c9f6089f1fbb3bd91377fa4f0a69b
Barros - Supero <barrosebs@gmail.com>
2023-11-10 20:43:58 -0300

    feat: Adicionando header e imagem perfil

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { expand } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  menuExpandido: boolean = false;
+  hidden = true;
+  icon = 'fa-solid fa-bars';
+  constructor() {}
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+  }
+  async delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  async toggleMenu() {
+    this.menuExpandido = !this.menuExpandido;
+    if (this.hidden) {
+      await this.delay(300);
+      this.hidden = false;
+      this.icon = 'fa-light fa-xmark';
+    } else {
+      this.hidden = true;
+      this.icon = 'fa-solid fa-bars';
+    }
+  }
 }
