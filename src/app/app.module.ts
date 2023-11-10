836bd21fdef94f99c8b5bf07f601aa14aea91d25
Barros - Supero <barrosebs@gmail.com>
2023-11-10 20:38:31 -0300

    chore: adicionando o CommonModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
-import { NavComponent } from './components/layout/nav/nav.component';
+import { NavComponent } from './components/layout/nav/NavComponent';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    NavComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, NavComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, CommonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
