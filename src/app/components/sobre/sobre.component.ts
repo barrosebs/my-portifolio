41d8145b53c84b8b8d8a9b41c8d99461ed23aab4
Barros - Supero <barrosebs@gmail.com>
2023-11-13 21:20:03 -0300

    refactor: ajustando o sobre

--- src/app/components/sobre/sobre.component.ts
+++ src/app/components/sobre/sobre.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
-  styleUrls: ['./sobre.component.scss']
+  styleUrls: ['./sobre.component.scss'],
 })
 export class SobreComponent {
+  dataNascimento: Date = new Date('1977-2-27');
+  hoje = new Date();
+  calcularIdade(): number {
+    const hoje = new Date();
+    const nascimento = new Date(this.dataNascimento);
 
+    let idade = hoje.getFullYear() - nascimento.getFullYear();
+
+    // Verificar se o aniversário já ocorreu este ano
+    if (
+      hoje.getMonth() < nascimento.getMonth() ||
+      (hoje.getMonth() === nascimento.getMonth() &&
+        hoje.getDate() < nascimento.getDate())
+    ) {
+      idade--;
+    }
+
+    return idade;
+  }
 }
