84180e87538ed982141b198e4cb9778182dfcde5
Barros - Supero <barrosebs@gmail.com>
2023-11-14 11:47:54 -0300

    feat: habilidades e progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,13 @@ import { NavComponent } from './components/layout/nav/nav.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { SobreComponent } from './components/sobre/sobre.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { CurriculoComponent } from './components/curriculo/curriculo.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavComponent, FooterComponent, HomeComponent, SobreComponent],
+  declarations: [AppComponent, HeaderComponent, NavComponent, FooterComponent, HomeComponent, SobreComponent, HabilidadesComponent, ProgressBarComponent, CurriculoComponent, PortfolioComponent],
   imports: [BrowserModule, AppRoutingModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
