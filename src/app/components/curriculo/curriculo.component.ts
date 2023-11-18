b70fa9182a05ea7bf67967bf2ea3f0f8a047ce5d
Barros - Supero <barrosebs@gmail.com>
2023-11-17 23:19:56 -0300

    feat: botão para imprimir curriculo

--- src/app/components/curriculo/curriculo.component.ts
+++ src/app/components/curriculo/curriculo.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-curriculo',
   templateUrl: './curriculo.component.html',
-  styleUrls: ['./curriculo.component.scss']
+  styleUrls: ['./curriculo.component.scss'],
 })
 export class CurriculoComponent {
+  printCurriculo(div: string): void {
+    let divCurriculo = document.getElementById(div)?.innerHTML;
 
+    let a = window.open('', '', 'height=500, width=500');
+    a?.document.write('<html>');
+    a?.document.write(
+      '<body > <h2>Eduardo Barros</h2><h4>WhatsApp: 71996407566</h4> <h4>Email: barrosebs@gmail.com</h4>' +
+        divCurriculo
+    );
+    a?.document.write(divCurriculo !== undefined ? toString() : '');
+    a?.document.write('</body></html>');
+    a?.print();
+    a?.document.close();
+  }
 }
