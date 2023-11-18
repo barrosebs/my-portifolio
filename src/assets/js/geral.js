eaee4635ce553beeb4e4c465ae5dc4e1c9a46767
Barros - Supero <barrosebs@gmail.com>
2023-11-17 21:19:46 -0300

    chore: removendo console.log

--- src/assets/js/geral.js
+++ src/assets/js/geral.js
@@ -1,10 +1,7 @@
 const animeScroll = () => {
   const item = document.querySelectorAll("[data-anime]");
-  console.log(item);
   const windowTop = window.pageYOffset + window.innerHeight * 0.85;
-  console.log(windowTop);
   item.forEach((element) => {
-    console.log(element);
     if (windowTop > element.offsetTop) {
       element.classList.add("animate");
     } else {
