0615badd2631961e5accd9eb8ae4e593e9917dc1
Barros - Supero <barrosebs@gmail.com>
2023-11-22 18:13:20 -0300

    feat: add icon para home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  rolagemTop = window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.menu');
+    const higth = window.pageYOffset + window.innerHeight * 0.85;
+    if (higth > 1200) {
+      navbar?.classList.add('rolagem');
+    } else {
+      navbar?.classList.remove('rolagem');
+    }
+  });
 }
