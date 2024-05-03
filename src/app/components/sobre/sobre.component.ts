b4c4c8b493993e87ae0017ef4b6a1b48a83c3d80
Eduardo Barros Dos Santos <barrosebs@gmail.com>
2024-05-03 09:31:07 -0300

    calculo de experiência

--- src/app/components/sobre/sobre.component.ts
+++ src/app/components/sobre/sobre.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./sobre.component.scss'],
 })
 export class SobreComponent {
+  StartExperiencia: Date = new Date('2017-03-01')
   dataNascimento: Date = new Date('1977-2-27');
   calcularIdade(): number {
     const hoje = new Date();
     const nascimento = new Date(this.dataNascimento);
-
     let idade = hoje.getFullYear() - nascimento.getFullYear();
 
     // Verificar se o aniversário já ocorreu este ano
@@ -23,5 +23,23 @@ export class SobreComponent {
     }
 
     return idade;
+
+  }
+  calcularExperiencia(): number {
+    const hoje = new Date();
+    const experiencia = new Date(this.StartExperiencia);
+    let AnoExperiencia = hoje.getFullYear() - experiencia.getFullYear();
+
+    // Verificar se o aniversário já ocorreu este ano
+    if (
+      hoje.getMonth() < experiencia.getMonth() ||
+      (hoje.getMonth() === experiencia.getMonth() &&
+        hoje.getDate() < experiencia.getDate())
+    ) {
+      AnoExperiencia--;
+    }
+
+    return AnoExperiencia;
+
   }
 }
